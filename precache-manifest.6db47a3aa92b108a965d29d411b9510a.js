6608bae3e1587aa65b6541ac980dffdabe891e64
huanghaijun <huanghaijun@itouchtv.cn>
2019-03-02 17:58:30 +0800

    更新..：2019-3-2 17:58:25

---
+++ precache-manifest.6db47a3aa92b108a965d29d411b9510a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eec04dedfb20d54518d3",
+    "url": "/static/js/main.eec04ded.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "eec04dedfb20d54518d3",
+    "url": "/static/css/main.35abe5b1.chunk.css"
+  },
+  {
+    "revision": "365a5d9c429aabc4e81b4f059bf99249",
+    "url": "/index.html"
+  }
+];
