bf00fcf852369e47a293e5231ecd7abd4eb36fce
huanghaijun <huanghaijun@itouchtv.cn>
2019-03-02 18:34:09 +0800

    eject before

--- ghpages.js
+++ ghpages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'build',
     {
         branch: 'master',
-        repo: 'https://github.com/salonnlee/salonnlee.github.io.git',
+        repo: 'git@github.com:salonnlee/salonnlee.github.io.git',
         message: '更新：' + new Date().toLocaleString()
     },
     err => {
