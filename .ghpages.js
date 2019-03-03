27b1470273fc0ebd3fbce851df7677a6923335fd
salonnlee <huanghj199512@163.com>
2019-03-04 01:24:40 +0800

    back to https

--- .ghpages.js
+++ .ghpages.js
@@ -3,8 +3,12 @@ const ghpages = require('gh-pages');
 ghpages.publish(
     'dist',
     {
+        user: {
+            name: 'salonnlee',
+            email: 'huanghj199512@163.com'
+        },
         branch: 'master',
-        repo: 'git@github.com:salonnlee/salonnlee.github.io.git',
+        repo: 'https://github.com/salonnlee/salonnlee.github.io.git',
         message: '更新：' + new Date().toLocaleString()
     },
     err => {
