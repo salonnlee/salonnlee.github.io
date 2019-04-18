715352344ff5ef81f499ad9f8764853e88566e2f
salonnlee <huanghj199512@163.com>
2019-04-19 00:16:29 +0800

    Entrance route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Entrance from './views/Entrance';
 
 class App extends Component {
     render() {
-        return <div className="App" />;
+        return (
+            <Router>
+                <Route path="/" exact component={Entrance} />
+            </Router>
+        );
     }
 }
 
