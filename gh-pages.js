69c1197172544626f991205b8cee66d8e8d3d089
salonnlee <huanghj199512@163.com>
2019-05-03 20:17:33 +0800

    email fixed

--- gh-pages.js
+++ gh-pages.js
@@ -1,20 +1,20 @@
-const ghpages = require('gh-pages');
+const ghpages = require("gh-pages");
 
 ghpages.publish(
-    'build',
+    "build",
     {
         user: {
-            name: 'salonnlee',
-            emial: 'huanghj199512@163.com'
+            name: "salonnlee",
+            email: "huanghj199512@163.com"
         },
-        branch: 'master',
-        repo: 'https://github.com/salonnlee/salonnlee.github.io.git',
-        message: '更新：' + new Date().toLocaleString()
+        branch: "master",
+        repo: "https://github.com/salonnlee/salonnlee.github.io.git",
+        message: "更新：" + new Date().toLocaleString()
     },
     err => {
         if (err) {
-            console.log('Gh Pages Publish Failed, ', err);
+            console.log("Github Pages Publish Failed, ", err);
         }
-        console.log('Gh Pages Publish Success.');
+        console.log("Github Pages Publish Success.");
     }
 );
