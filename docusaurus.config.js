f1e8a34ca2654123e28c143c87ab2f37b63db19f
huanghaijun <huanghaijun@itouchtv.cn>
2021-02-18 17:22:36 +0800

    docusaurus deploy to gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://salonnlee.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'salonnlee', // Usually your GitHub org/user name.
+  projectName: 'salonnlee.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
