06d7f47c41c99225001756b8dcf6b3a4da9c5370
huanghaijun <huanghaijun@itouchtv.cn>
2021-06-14 11:59:57 +0800

    Home page redirect to Notion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,33 @@
 import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import "./index.css";
 import { Helmet } from "react-helmet";
 
 export default function Home() {
+  // goto notion
+  if (ExecutionEnvironment.canUseDOM) {
+    window.location.href =
+      "https://www.notion.so/SALONNLEE-6d4970b31bec47db898a832ae60cdc8d";
+  }
   const { siteConfig } = useDocusaurusContext();
   return (
     <React.Fragment>
       <Helmet>
         <title>{siteConfig.title}</title>
       </Helmet>
-      <div class="solar-syst">
-        <div class="sun"></div>
-        <div class="mercury"></div>
-        <div class="venus"></div>
-        <div class="earth"></div>
-        <div class="mars"></div>
-        <div class="jupiter"></div>
-        <div class="saturn"></div>
-        <div class="uranus"></div>
-        <div class="neptune"></div>
-        <div class="pluto"></div>
-        <div class="asteroids-belt"></div>
+      <div className="solar-syst">
+        <div className="sun"></div>
+        <div className="mercury"></div>
+        <div className="venus"></div>
+        <div className="earth"></div>
+        <div className="mars"></div>
+        <div className="jupiter"></div>
+        <div className="saturn"></div>
+        <div className="uranus"></div>
+        <div className="neptune"></div>
+        <div className="pluto"></div>
+        <div className="asteroids-belt"></div>
       </div>
     </React.Fragment>
   );
