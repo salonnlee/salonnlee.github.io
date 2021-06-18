7a2d2a19a34fc0584ed634b9aa14f3bf0bf45cf6
huanghaijun <huanghaijun@itouchtv.cn>
2021-06-18 15:54:24 +0800

    清空 Page index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,18 @@
 import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import "./index.css";
 import { Helmet } from "react-helmet";
 
 export default function Home() {
-  // goto notion
-  if (ExecutionEnvironment.canUseDOM) {
-    window.location.href =
-      "https://www.notion.so/SALONNLEE-6d4970b31bec47db898a832ae60cdc8d";
-  }
   const { siteConfig } = useDocusaurusContext();
   return (
     <React.Fragment>
       <Helmet>
         <title>{siteConfig.title}</title>
       </Helmet>
-      <div className="solar-syst">
-        <div className="sun"></div>
-        <div className="mercury"></div>
-        <div className="venus"></div>
-        <div className="earth"></div>
-        <div className="mars"></div>
-        <div className="jupiter"></div>
-        <div className="saturn"></div>
-        <div className="uranus"></div>
-        <div className="neptune"></div>
-        <div className="pluto"></div>
-        <div className="asteroids-belt"></div>
+      <div id="app">
+        <div className="center-box">-</div>
       </div>
     </React.Fragment>
   );
