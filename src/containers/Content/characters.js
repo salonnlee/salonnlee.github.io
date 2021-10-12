8d353c39327f404166ff2152d811504bb8c4aaa4
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-10-12 17:23:41 +0800

    Soliloquy

---
+++ src/containers/Content/characters.js
@@ -0,0 +1,6 @@
+const CHARACTERS = [
+  "Swimming out till the sea turns blue..",
+  "一直游到海水变蓝.. 🌊"
+];
+
+export default CHARACTERS;
--- src/containers/Content/index.js
+++ src/containers/Content/index.js
@@ -1,10 +1,23 @@
 import React from "react";
+import CHARACTERS from "./characters";
 
 import "./index.css";
 
+const Soliloquy = () => (
+  <div className="soliloquy">
+    {CHARACTERS.map((characters) => {
+      return <p>{characters}</p>;
+    })}
+  </div>
+);
+
 class Content extends React.Component {
   render() {
-    return <div className="layout-content"></div>;
+    return (
+      <div className="layout-content">
+        <Soliloquy />
+      </div>
+    );
   }
 }
 
