a81b00c63b32ecbd05b73afc8ebd5f2340128427
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-10-21 10:23:08 +0800

    Update BILLS

--- src/containers/Content/characters.js
+++ src/containers/Content/characters.js
@@ -1,6 +1,25 @@
+export const LINEBREAK = "@@LINEBREAK@@";
+
 const CHARACTERS = [
   "Swimming out till the sea turns blue..",
   "一直游到海水变蓝.. 🌊"
+];
+
+export const BILLS = [
+  "2021-11-04 - ¥3617.54",
+  "2021-12-05 - ¥8636.98",
+  "2021-12-04 - ¥3627.54",
+  LINEBREAK,
+  "¥8636.98 + ¥39470.75 = ¥48107.73 | Total Debt",
+  LINEBREAK,
+  "2022-02-01 Spring",
+  LINEBREAK,
+  "(¥30.00 × 20) + [(¥30.00 + ¥30.00) × 20] + (¥300.00 × 4) + ¥1750.00 = ",
+  "¥600.00 + ¥1200.00 + ¥1200.00 + ¥1750.00 = ¥4750.00 | Monthly",
+  "Car + Eat + Medicine + House = Monthly",
+  LINEBREAK,
+  "(¥15000.00 - ¥4750.00) × 3 = ¥10250.00 × 3 = ¥30750.00 | Left",
+  "¥48107.73 - ¥30750.00 = 17357.73 | Debt"
 ];
 
 export default CHARACTERS;
