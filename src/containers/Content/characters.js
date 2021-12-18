3d156e2e98853aac6ac77cd01b98a66e380d70ac
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-12-18 11:33:40 +0800

    Update debt

--- src/containers/Content/characters.js
+++ src/containers/Content/characters.js
@@ -10,14 +10,14 @@ const SPACECHARACTER = "&nbsp;";
 export const BILLS = [
   SPACECHARACTER.repeat(16) + "Debt adjustment",
   SPACECHARACTER.repeat(9) +
-    "Alipay -¥10000.00 + -¥10000.00 + -¥20000.00 = -¥40000.00",
+    "Alipay -¥5000.00 + -¥20000.00 = -¥25000.00",
   LINEBREAK,
-  SPACECHARACTER.repeat(16) + "2021-12-15",
+  SPACECHARACTER.repeat(16) + "2021-01-15",
   SPACECHARACTER.repeat(9) + "Salary +¥14750.00",
   SPACECHARACTER.repeat(0) +
-    "Monthly Expense -[(¥15.00 + ¥15.00) × 20] + -[(¥15.00 + ¥30.00 + ¥30.00) × 20] + -(¥200.00 × 4) + -(¥128.00 + ¥328.00) + -¥1750.00 = ",
+    "Monthly Expense -[(¥15.00 + ¥15.00) × 20] + -[(¥15.00 + ¥30.00 + ¥30.00) × 30] + -(¥648.00) + -¥1750.00 = ",
   SPACECHARACTER.repeat(16) +
-    "-¥600.00 + -¥1500.00 + -¥800.00 + -¥500.00 + -¥1750.00 = -¥5150.00",
+    "-¥600.00 + -¥2250.00 + -¥648.00 + -¥1750.00 = -¥5250.00",
   SPACECHARACTER.repeat(16) +
     [
       SPACECHARACTER.repeat(3),
@@ -28,10 +28,6 @@ export const BILLS = [
       "Meals",
       SPACECHARACTER.repeat(3),
       "+",
-      SPACECHARACTER.repeat(1),
-      "Medicine",
-      SPACECHARACTER.repeat(1),
-      "+",
       SPACECHARACTER.repeat(3),
       "Game",
       SPACECHARACTER.repeat(3),
@@ -40,22 +36,11 @@ export const BILLS = [
       "House"
     ].join(""),
   SPACECHARACTER.repeat(0) +
-    "Residual salary +¥14750.00 + -¥5150.00 = +¥10000.00",
+    "Residual salary +¥14750.00 + -¥5250.00 = +¥10000.00",
   LINEBREAK,
   SPACECHARACTER.repeat(16) + "Debt adjustment",
   SPACECHARACTER.repeat(9) +
-    "Alipay -¥10000.00 + -¥10000.00 + -¥20000.00 + +¥10000.00 = -¥30000.00",
-  LINEBREAK,
-  SPACECHARACTER.repeat(16) + "2022-01-15",
-  SPACECHARACTER.repeat(9) + "Salary +¥14750.00",
-  SPACECHARACTER.repeat(0) +
-    "Monthly Expense -¥600.00 + -¥1500.00 + -¥800.00 + -¥500.00 + -¥1750.00 = -¥5150.00",
-  SPACECHARACTER.repeat(0) +
-    "Residual salary +¥14750.00 + -¥5150.00 = +¥10000.00",
-  LINEBREAK,
-  SPACECHARACTER.repeat(16) + "Debt adjustment",
-  SPACECHARACTER.repeat(9) +
-    "Alipay -¥10000.00 + -¥20000.00 + +¥10000.00 = -¥20000.00",
+    "Alipay -¥5000.00 + -¥20000.00 + +¥10000.00 = -¥15000.00",
   LINEBREAK,
   SPACECHARACTER.repeat(16) + "2022-02-01 Spring"
 ];
