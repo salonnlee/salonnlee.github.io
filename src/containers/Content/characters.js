f13b1d34fa1e86a7ceabc49f31d58dd14a521a20
huanghaijun <cloud_huanghaiyun@youzan.com>
2022-02-10 17:55:02 +0800

    Update debt

--- src/containers/Content/characters.js
+++ src/containers/Content/characters.js
@@ -9,14 +9,14 @@ const SPACECHARACTER = "&nbsp;";
 
 export const BILLS = [
   SPACECHARACTER.repeat(16) + "Debt adjustment",
-  SPACECHARACTER.repeat(9) + "Alipay -¥10000.00 = -¥10000.00",
+  SPACECHARACTER.repeat(9) + "Alipay -¥10000.00 + -¥10000.00 = -¥20000.00",
   LINEBREAK,
   SPACECHARACTER.repeat(16) + "2022-02-15",
   SPACECHARACTER.repeat(9) + "Salary +¥14750.00",
   SPACECHARACTER.repeat(0) +
-    "Monthly Expense -[(¥15.00 + ¥15.00) × 20] + -[(¥15.00 + ¥30.00 + ¥30.00) × 30] + -¥648.00 + -¥1750.00 = ",
+    "Monthly Expense -[(¥15.00 + ¥15.00) × 20] + -[(¥15.00 + ¥30.00 + ¥30.00) × 20] + -¥328.00 + -¥3500.00 = ",
   SPACECHARACTER.repeat(16) +
-    "-¥600.00 + -¥2250.00 + -¥648.00 + -¥1750.00 = -¥5250.00",
+    "-¥600.00 + -¥1500.00 + -¥328.00 + -¥3500.00 = -¥6000.00",
   SPACECHARACTER.repeat(16) +
     [
       SPACECHARACTER.repeat(3),
@@ -35,7 +35,17 @@ export const BILLS = [
       "House"
     ].join(""),
   SPACECHARACTER.repeat(0) +
-    "Residual salary +¥14750.00 + -¥5250.00 = +¥10000.00",
+    "Residual salary +¥14750.00 + -¥6000.00 = +¥10000.00",
+  LINEBREAK,
+  SPACECHARACTER.repeat(16) + "Debt adjustment",
+  SPACECHARACTER.repeat(9) + "Alipay -¥20000.00 + +¥10000.00 = -¥10000.00",
+  LINEBREAK,
+  SPACECHARACTER.repeat(16) + "2022-03-15",
+  SPACECHARACTER.repeat(9) + "Salary +¥14750.00",
+  SPACECHARACTER.repeat(0) +
+    "Monthly Expense -¥600.00 + -¥1500.00 + -¥328.00 + -¥3500.00 = -¥6000.00",
+  SPACECHARACTER.repeat(0) +
+    "Residual salary +¥14750.00 + -¥6000.00 = +¥10000.00",
   LINEBREAK,
   SPACECHARACTER.repeat(16) + "Debt adjustment",
   SPACECHARACTER.repeat(9) + "Alipay -¥10000.00 + +¥10000.00 = ¥0.00"
